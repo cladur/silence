1454bcbc0f0d6580697c8b5a8dbc478f9c5cb60d
Merloran <poncyleuszmaciej@gmail.com>
2023-05-21 00:43:25 +0200

    Added animation speed slider

--- managers/animation/ecs/animation_instance.cpp
+++ managers/animation/ecs/animation_instance.cpp
@@ -23,6 +23,7 @@ void AnimationInstance::serialize_json(nlohmann::json &serialized_scene) {
 	nlohmann::json::object_t serialized_component;
 	ResourceManager &resource_manager = ResourceManager::get();
 	serialized_component["animation_name"] = resource_manager.get_animation_name(animation_handle);
+	serialized_component["ticks_per_second"] = ticks_per_second;
 	serialized_scene.push_back(nlohmann::json::object());
 	serialized_scene.back()["component_data"] = serialized_component;
 	serialized_scene.back()["component_name"] = "AnimationInstance";
@@ -30,6 +31,7 @@ void AnimationInstance::serialize_json(nlohmann::json &serialized_scene) {
 
 void AnimationInstance::deserialize_json(nlohmann::json &serialized_component) {
 	std::string animation_name = serialized_component["animation_name"];
+	ticks_per_second = serialized_component["ticks_per_second"];
 	ResourceManager &resource_manager = ResourceManager::get();
 	animation_handle = resource_manager.load_animation(animation_name.c_str());
 }
