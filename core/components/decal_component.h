90d258f5f45f810f1b1fb93468b54a73382ab43e
Merloran <poncyleuszmaciej@gmail.com>
2023-06-22 04:01:58 +0200

    Added ao roughness and metallness to decals

--- core/components/decal_component.h
+++ core/components/decal_component.h
@@ -6,6 +6,11 @@
 struct Decal {
 	Handle<Texture> albedo;
 	Handle<Texture> normal;
+	Handle<Texture> ao_rough_metal;
+	bool has_normal = false;
+	bool has_ao = false;
+	bool has_roughness = false;
+	bool has_metalness = false;
 	glm::vec4 color = glm::vec4(1.0f);
 
 	void serialize_json(nlohmann::json &serialized_scene) {
@@ -13,10 +18,15 @@ struct Decal {
 		nlohmann::json::object_t serialized_component;
 		serialized_component["albedo"] = rm.get_texture_name(albedo);
 		serialized_component["normal"] = rm.get_texture_name(normal);
+		serialized_component["ao_rough_metal"] = rm.get_texture_name(ao_rough_metal);
 		serialized_component["color"]["r"] = color.r;
 		serialized_component["color"]["g"] = color.g;
 		serialized_component["color"]["b"] = color.b;
 		serialized_component["color"]["a"] = color.a;
+		serialized_component["has_normal"] = has_normal;
+		serialized_component["has_ao"] = has_ao;
+		serialized_component["has_roughness"] = has_roughness;
+		serialized_component["has_metalness"] = has_metalness;
 		serialized_scene.push_back(nlohmann::json::object());
 		serialized_scene.back()["component_data"] = serialized_component;
 		serialized_scene.back()["component_name"] = "Decal";
@@ -32,6 +42,12 @@ struct Decal {
 		if (!normal_name.empty()) {
 			normal = rm.load_texture(normal_name.c_str());
 		}
+		if (serialized_component.contains("ao_rough_metal")) {
+			std::string ao_rough_metal_name = serialized_component["ao_rough_metal"];
+			if (!normal_name.empty()) {
+				ao_rough_metal = rm.load_texture(ao_rough_metal_name.c_str());
+			}
+		}
 		if (serialized_component.contains("color")) {
 			color.r = serialized_component["color"]["r"];
 			color.g = serialized_component["color"]["g"];
@@ -39,6 +55,26 @@ struct Decal {
 			color.a = serialized_component["color"]["a"];
 		} else {
 			color = glm::vec4(1.0f);
+		}
+		if (serialized_component.contains("has_normal")) {
+			has_normal = serialized_component["has_normal"];
+		} else {
+			has_normal = false;
+		}
+		if (serialized_component.contains("has_ao")) {
+			has_ao = serialized_component["has_ao"];
+		} else {
+			has_ao = false;
+		}
+		if (serialized_component.contains("has_roughness")) {
+			has_roughness = serialized_component["has_roughness"];
+		} else {
+			has_roughness = false;
+		}
+		if (serialized_component.contains("has_metalness")) {
+			has_metalness = serialized_component["has_metalness"];
+		} else {
+			has_metalness = false;
 		}
 	}
 };
